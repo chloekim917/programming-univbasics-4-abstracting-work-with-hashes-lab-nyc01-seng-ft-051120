0c68e1459fcbb90462271d8690b91cd09de75b38
Seoyoung Kim <mintholic1@gmail.com>
2020-04-23 18:04:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,6 +4,6 @@ end
 
 def read_from_hash(hash, key)
   hash[:key]
-  # return the correct value using the hash and key parameters
+  # return the correct value using the  key parametershash and
 end
 
