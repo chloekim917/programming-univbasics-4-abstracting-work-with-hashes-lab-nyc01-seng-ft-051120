3684deb69746171c3fea89d0514e7c1a0cf71225
Seoyoung Kim <mintholic1@gmail.com>
2020-04-23 18:03:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -2,3 +2,8 @@ def my_hash_creator(key, value)
   new_hash = {key => value}
 end
 
+def read_from_hash(hash, key)
+  hash[:key]
+  # return the correct value using the hash and key parameters
+end
+
